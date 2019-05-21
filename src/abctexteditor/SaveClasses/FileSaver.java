585623ea1712e8f7061b7386978b73680a260392
jusola2 <juanjosesolano@hotmail.com>
2019-05-21 11:22:02 -0600

    Nueva interfaz
    Interfaz para las clases de guardado
    1. TXT simple: se guarda el texto en txt.
    2. TXT por tabuladores: cada 10 caracteres se separa por tabuladores. Al abrirlo, debe
    mostrarse sin tabuladores, solo como texto normal.
    3. XML: todo el texto en un solo tag. Los colores debe representarlos en otro tag.
    4. CSV: cada espacio se separa por “,” o “;” … al recuperar el texto se muestra simple, sin esos
    caracteres.
    5. PDF: solo guarda como PDF, no lo puede abrir.
    6. JSON: cada párrafo va en un Array de un Json. Al abrirlo, lo presenta normal, texto original

---
+++ src/abctexteditor/SaveClasses/FileSaver.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abctexteditor.SaveClasses;
+
+/**
+ *
+ * @author juanj
+ */
+public interface FileSaver {
+    public boolean SaveFile(String text);
+}
