1680a55b80131a475c0d9ed269a326168717129b
jusola2 <juanjosesolano@hotmail.com>
2019-05-22 11:49:58 -0600

    Little update

--- src/abctexteditor/Files/FileSaverPDF.java
+++ src/abctexteditor/Files/FileSaverPDF.java
@@ -1,10 +1,11 @@
+package abctexteditor.Files;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import abctexteditor.Files.IFileFormat;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
